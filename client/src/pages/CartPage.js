4825358bd865dc20c471497a46a07f17bfa8341e
Your Name <you@example.com>
2023-04-04 15:12:29 +0530

    first commit

---
+++ client/src/pages/CartPage.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import Layout from "./../components/Layout/Layout";
+import { useCart } from "../context/cart";
+import { useAuth } from "../context/auth";
+import { useNavigate } from "react-router-dom";
+import DropIn from "braintree-web-drop-in-react";
+import { AiFillWarning } from "react-icons/ai";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const CartPage = () => {
+  const [auth, setAuth] = useAuth();
+  const [cart, setCart] = useCart();
+  const [clientToken, setClientToken] = useState("");
+  const [instance, setInstance] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  //total price
+  const totalPrice = () => {
+    try {
+      let total = 0;
+      cart?.map((item) => {
+        total = total + item.price;
+      });
+      return total.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  //detele item
+  const removeCartItem = (pid) => {
+    try {
+      let myCart = [...cart];
+      let index = myCart.findIndex((item) => item._id === pid);
+      myCart.splice(index, 1);
+      setCart(myCart);
+      localStorage.setItem("cart", JSON.stringify(myCart));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  
+
+ 
+};
+
+export default CartPage;
